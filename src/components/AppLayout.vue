a292db4e1768f34c84e2acf19c25a1731e151c2c
codemaoz90 <codemaoz90@gmail.com>
2021-11-11 14:43:22 +0100

    Router created FAQ Home

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-layout">
+    <header class="header">
+      <div><img class="img" src="../assets/svg/logo.svg" /></div>
+      <div>My shirt shop</div>
+    </header>
+    <!-- Menu will be here -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.header {
+  .img {
+    width: 64px;
+    height: 64px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"
+import router from "./router"
+import AppLayout from "./components/AppLayout"
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(AppLayout),
+  router
+}).$mount("#app")
